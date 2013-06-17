c9680d7ed1a28df599b540c4dda828f529525e47
Cyril Bordreuil <cyril.bordreuil@univ-montp2.fr>
2013-06-17 13:28:15 +0200

    cb:Tp1

---
+++ Examples/A_PoutreEncastree/poutre.py
@@ -0,0 +1,105 @@
+# -*- coding: iso-8859-1 -*-
+
+###
+### This file is generated automatically by SALOME v6.5.0 with dump python functionality
+###
+
+import sys
+import salome
+
+salome.salome_init()
+theStudy = salome.myStudy
+
+import salome_notebook
+notebook = salome_notebook.notebook
+sys.path.insert( 0, r'/home/bordreuil/Enseignement/elementsFiNimes/Examples/A_PoutreEncastree')
+
+import iparameters
+ipar = iparameters.IParameters(salome.myStudy.GetCommonParameters("Interface Applicative", 1), True)
+
+
+ipar.append("AP_MODULES_LIST", "Geometry")
+ipar.append("AP_MODULES_LIST", "Mesh")
+
+
+
+###
+### GEOM component
+###
+
+import GEOM
+import geompy
+import math
+import SALOMEDS
+
+
+geompy.init_geom(theStudy)
+
+Sommet_1 = geompy.MakeVertex(0, 0, 0)
+[geomObj_1] = geompy.SubShapeAll(Sommet_1, geompy.ShapeType["VERTEX"])
+[geomObj_2] = geompy.SubShapeAll(Sommet_1, geompy.ShapeType["VERTEX"])
+
+Sommet_2 = geompy.MakeVertex(1000, 0, 0)
+[geomObj_3] = geompy.SubShapeAll(Sommet_2, geompy.ShapeType["VERTEX"])
+[geomObj_4] = geompy.SubShapeAll(Sommet_2, geompy.ShapeType["VERTEX"])
+
+Ligne_1 = geompy.MakeLineTwoPnt(Sommet_1, Sommet_2)
+
+listSubShapeIDs = geompy.SubShapeAllIDs(Ligne_1, geompy.ShapeType["VERTEX"])
+listSubShapeIDs = geompy.SubShapeAllIDs(Ligne_1, geompy.ShapeType["VERTEX"])
+listSameIDs = geompy.GetSameIDs(Ligne_1, geomObj_1)
+Encastre = geompy.CreateGroup(Ligne_1, geompy.ShapeType["VERTEX"])
+geompy.UnionIDs(Encastre, [2])
+
+listSameIDs = geompy.GetSameIDs(Ligne_1, geomObj_2)
+listSameIDs = geompy.GetSameIDs(Ligne_1, geomObj_3)
+Effort = geompy.CreateGroup(Ligne_1, geompy.ShapeType["VERTEX"])
+geompy.UnionIDs(Effort, [3])
+
+listSameIDs = geompy.GetSameIDs(Ligne_1, geomObj_4)
+geompy.addToStudy( Sommet_1, 'Sommet_1' )
+geompy.addToStudy( Sommet_2, 'Sommet_2' )
+geompy.addToStudy( Ligne_1, 'Ligne_1' )
+geompy.addToStudyInFather( Ligne_1, Encastre, 'Encastre' )
+geompy.addToStudyInFather( Ligne_1, Effort, 'Effort' )
+
+### Store presentation parameters of displayed objects
+import iparameters
+ipar = iparameters.IParameters(theStudy.GetModuleParameters("Interface Applicative", "GEOM", 1))
+
+#Set up entries:
+# set up entry GEOM_1 (Sommet_1) parameters
+objId = geompy.getObjectID(Sommet_1)
+
+###
+### SMESH component
+###
+
+import smesh, SMESH, SALOMEDS
+
+smesh.SetCurrentStudy(theStudy)
+import StdMeshers
+Maillage_1    = smesh.Mesh(Ligne_1)
+Regular_1D    = Maillage_1.Segment()
+Nb_Segments_1 = Regular_1D.NumberOfSegments(10)
+Nb_Segments_1.SetDistrType( 0 )
+isDone        = Maillage_1.Compute()
+Encastre_1    = Maillage_1.GroupOnGeom(Encastre,'Encastre',SMESH.NODE)
+Effort_1      = Maillage_1.GroupOnGeom(Effort,'Effort',SMESH.NODE)
+TOUT          = Maillage_1.GroupOnGeom(Ligne_1,'Ligne_1',SMESH.EDGE)
+TOUT.SetName( 'TOUT' )
+
+## set object names
+smesh.SetName(Maillage_1.GetMesh(), 'Maillage_1')
+smesh.SetName(Regular_1D.GetAlgorithm(), 'Regular_1D')
+smesh.SetName(Nb_Segments_1, 'Nb. Segments_1')
+smesh.SetName(Encastre_1, 'Encastre')
+smesh.SetName(Effort_1, 'Effort')
+smesh.SetName(TOUT, 'TOUT')
+
+
+
+
+if salome.sg.hasDesktop():
+  salome.sg.updateObjBrowser(1)
+  iparameters.getSession().restoreVisualState(1)
--- README.md
+++ README.md
@@ -1,4 +1,7 @@
 elementsFiNimes
 ===============
 
-Exemples de Salomeca - Code Aster réalisés dans les formations de Génie - Mécanique à Nîmes
+Exemples de Salomeca - Code Aster réalisés dans les formations de Génie - Mécanique à Nîmes.
+
+On retrouve une partie des tps issue au cours de la formations et une série de projets des étudiants de l'IUT GMP et de Polytech'Montpellier option Mécanique et Structures Industrielles.
+
